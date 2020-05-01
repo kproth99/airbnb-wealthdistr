f9f75c7290d258a7abaca02cbffa937e73eb98b5
Kieran Roth <rothki@bc.edu>
2020-05-01 19:27:53 -0400

    added docs

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,10 @@ module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'docs'),
   },
   devServer: {
-		contentBase: './dist'
+		contentBase: './docs'
 	},
   mode: 'development',
 	devtool: 'inline-source-map'
