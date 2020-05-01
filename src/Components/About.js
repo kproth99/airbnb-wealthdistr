72d63825c7d5c04600326bd7b77076d43c8834ea
Kieran Roth <rothki@bc.edu>
2020-05-01 16:35:46 -0400

    commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,11 +5,12 @@ export default function renderAbout(about) {
             ` <section id="about">
             <h1 class="animated infinite flash delay-3s">Airbnb Price and Wealth Distribution</h1>
             <div id="about" class="row">
+                    
                     <div class="col-6">
+                    <strong> Meet our Group Members!</strong><br>
                         <img class="profile-img" src="${sample(about.photos)}" width="200" height="300">
                         <p>
-                            <strong> This is our project about Airbnb and Price Distribution</strong><br>
-                            rothki@bc.edu<br>
+                            
                             140 Commonwealth Avenue, Chestnut Hill, MA 02467<br>
                             <a href="assets/Kieran Roth Resume.pdf" target="_blank"><i class="far fa-file-alt"></i> Resume | </a>
                             <a href="https://www.instagram.com/kproth99/" target="_blank">
