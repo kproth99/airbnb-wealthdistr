30f5c3548ad12c8a7bdbd900441a16d0b0cd4744
Kieran Roth <rothki@bc.edu>
2020-05-02 21:07:18 -0400

    image change

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -14,31 +14,12 @@ export function renderProjectItems(projects){
         d => `
 
         <div>
+        <div class="column2">
+        <img class="profile-img" src="${d.hoto1}" >
             <div class="project-title">
             <a href="?project=${d.id}"><strong>${d.title}</strong></a>
             </div>
         </div>
-
-        <div class="row">
-        <div class="col-6">
-            <div class="authors" style="text-align:left"> 
-                ${d.authors} 
-            </div>
-            <div class="source" style="text-align:left"> 
-                <em>${d.source}</em> 
-            </div>
-            
-  
-            <div class="label" style="text-align:left">
-                <span><a href="${d.materials[0].path}"> <i class="${d.icon1}"></i> 
-                    ${d.materials[0].label} </a>
-                </span> |
-                <span><a href="${d.materials[1].path}"> <i class="${d.icon2}"></i> 
-                    ${d.materials[1].label} </a>
-                </span>
-            </div>
-            
-  
         </div>
         </div>
     <br>`
